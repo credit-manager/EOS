48a803b66922e9dcfd1672aae767e946f8262f76
credit-manager <fc.creditmanager@gmail.com>
2026-09-04 23:43:49 +0300

    grant least-privilege defaults to PostgreSQL app role

--- docker/postgres/init/01-create-app-role.sh
+++ docker/postgres/init/01-create-app-role.sh
@@ -9,4 +9,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" \
   --set=app_password="$EOS_DB_APP_PASSWORD" <<'SQL'
 SELECT format('CREATE ROLE %I LOGIN PASSWORD %L NOSUPERUSER NOBYPASSRLS', :'app_user', :'app_password')
 WHERE NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = :'app_user')\gexec
+
+SELECT format('GRANT CONNECT ON DATABASE %I TO %I', current_database(), :'app_user')\gexec
+SELECT format('GRANT USAGE ON SCHEMA public TO %I', :'app_user')\gexec
+SELECT format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO %I', current_user, :'app_user')\gexec
+SELECT format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO %I', current_user, :'app_user')\gexec
 SQL
