ea9afd7e8264605a02f8f19f957035700262646d
credit-manager <fc.creditmanager@gmail.com>
2026-09-06 16:32:45 +0300

    feat: add v2 metadata kernel boundary

---
+++ eos_v2/domain/metadata/__init__.py
@@ -0,0 +1,5 @@
+"""Metadata kernel domain primitives."""
+
+from .entities import EntityDefinition, FieldDefinition, FieldType, RelationshipDefinition
+
+__all__ = ["EntityDefinition", "FieldDefinition", "FieldType", "RelationshipDefinition"]
