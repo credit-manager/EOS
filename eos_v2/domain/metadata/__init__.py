3a338003c837f922a3fd93647d26b208e6a0897e
credit-manager <fc.creditmanager@gmail.com>
2026-09-06 16:35:37 +0300

    refactor(v2): export dynamic record domain primitives

--- eos_v2/domain/metadata/__init__.py
+++ eos_v2/domain/metadata/__init__.py
@@ -1,5 +1,10 @@
 """Metadata kernel domain primitives."""
 
 from .entities import EntityDefinition, FieldDefinition, FieldType, RelationshipDefinition
+from .record_validation import RecordValidationError, validate_record
+from .records import DynamicRecord
 
-__all__ = ["EntityDefinition", "FieldDefinition", "FieldType", "RelationshipDefinition"]
+__all__ = [
+    "EntityDefinition", "FieldDefinition", "FieldType", "RelationshipDefinition",
+    "DynamicRecord", "RecordValidationError", "validate_record",
+]
